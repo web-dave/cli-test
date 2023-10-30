9a85506b2c7144f355917c540c23d534a3b27e10
Dave <dave@webdave.de>
2023-10-30 21:10:17 +0100

    ng0500 error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { file } from './file';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'cli-test';
+  numbers = ['1', '2m', '3m', '4', '5', '6m', '7'];
+
+  keys = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
+  keyMap: { [key: string]: string[] } = {
+    C: ['C', 'Dm', 'Em', 'F', 'G', 'Am', 'B'],
+    D: ['D', 'Em', 'F#m', 'G', 'A', 'Bm', 'C#'],
+    E: ['E', 'F#m', 'G#m', 'A', 'B', 'C#m', 'D#'],
+    F: ['F', 'Gm', 'Am', 'Bb', 'C', 'Dm', 'E'],
+    G: ['G', 'Am', 'Bm', 'C', 'D', 'Em', 'F#'],
+    A: ['A', 'Bm', 'C#m', 'D', 'E', 'F#m', 'G#'],
+    B: ['B', 'C#m', 'D#m', 'E', 'F#', 'G#m', 'A#'],
+  };
+  selectedKey = signal('D');
+  selectKey(e: string) {
+    this.selectedKey.update(() => e);
+    console.log(e);
+  }
+
+  file = signal(file.split('\n'));
+
+  text = computed(() => {
+    return this.file();
+  });
 }
---
+++ src/app/file.ts
@@ -0,0 +1,16 @@
+export const file: string = `
+#6m               #2m
+ I can buy myself flowers
+#5                     #1   #1/#7
+ Write my name in the sand
+#6m                  #2m
+ Talk to myself for hours
+#5                           #1   #1/#7
+ Say things you don't understand
+#6m                 #2m
+ I can take myself dancing
+#5                      #C   #C/#B
+ And I can hold my own hand
+            #4              #37       #6m
+Yeah, I can love me better than you can
+`;
